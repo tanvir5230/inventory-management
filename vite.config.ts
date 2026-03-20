25286b3605b8ee2c47ab199b7178d7e5cba8ade4
tanvir-ibn-touhid <tanvir.touhid@technovativesolutions.co.uk>
2026-03-20 12:19:21 +0600

    chore: updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
@@ -5,4 +6,14 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true, // clear the dist folder before building
+    chunkSizeWarningLimit: 1000,
+  },
 });
